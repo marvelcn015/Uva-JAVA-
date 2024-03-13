fe3c546bec257f05f473e430bf2ade76791e243a
Aaron Yeh <137107162+marvelcn015@users.noreply.github.com>
2024-03-13 17:03:46 +0800

    Create 00299 - Train Swapping.java

---
+++ 00299 - Train Swapping.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int set = sc.nextInt();
+	while(set-- > 0){
+		int L = sc.nextInt();
+		int[] arr = new int[L];
+		for(int i = 0; i<arr.length;i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		int count = 0;
+		for(int i = 0; i<arr.length-1;i++){
+			for(int j = 0; j<arr.length-1-i;j++){
+				if(arr[j]>arr[j+1]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				count++;
+				}
+			}
+		}
+		
+		System.out.println("Optimal train swapping takes "+count+" swaps.");
+		}
+	}
+}
