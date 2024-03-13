6b459216c1529be49386053270996271ebb1857a
Aaron Yeh <137107162+marvelcn015@users.noreply.github.com>
2024-03-14 03:26:00 +0800

    Update 10035 - Primary Arithmetic.java

--- 10035 - Primary Arithmetic.java
+++ 10035 - Primary Arithmetic.java
@@ -1,45 +1,40 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            long a = sc.nextLong();
-            long b = sc.nextLong();
+		while (sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if (a == 0 && b == 0)
+				return;
 
-            if (a == 0 & b == 0) {
-                return;
-            }
+			int count = 0;
+			boolean flag = false;
+			int temp = 0;
+			for (; a > 0 || b > 0; a /= 10, b /= 10) {
 
-            int CO = 0;
-            boolean flag = false;
+				if (flag) {
+					temp = a % 10 + b % 10 + 1;
+					flag = false;
+				} else {
+					temp = a % 10 + b % 10;
+				}
 
-            for (int i = 10; a > 0 | b > 0; a /= 10, b /= 10) {
-                if (flag) {
-                    if (a % i + b % i + 1 >= 10) {
-                        CO++;
-                        flag = true;
-                    } else
-                        flag = false;
+				if (temp >= 10) {
+					flag = true;
+					count++;
+				}
+			}
 
-                } else {
-                    if (a % i + b % i >= 10) {
-                        CO++;
-                        flag = true;
-                    } else
-                        flag = false;
-                }
-            }
-
-            if (CO == 0) {
-                System.out.println("No carry operation.");
-            } else if (CO == 1) {
-                System.out.println(1 + " carry operation.");
-            } else {
-                System.out.println(CO + " carry operations.");
-            }
-
-        }
-    }
+			if (count == 0) {
+				System.out.println("No carry operation.");
+			} else if (count == 1) {
+				System.out.println("1 carry operation.");
+			} else {
+				System.out.println(count + " carry operations.");
+			}
+		}
+	}
 }
